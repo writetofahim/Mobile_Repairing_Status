659735ba50cc6c42dd660baf214e46e5b1d210ef
writetofahim <writetofahimahammad@gmail.com>
2023-07-09 13:22:10 +0600

    save invoice done but show invoice is not done

--- api/models/Status.js
+++ api/models/Status.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const statusSchema = new mongoose.Schema({
   customerId: { type: String, required: true },
   status: { type: String },
+  invoice: { type: Array, default: [] },
 });
 
 module.exports = mongoose.model("Status", statusSchema);
