f7a35a4da72d55441d02919bbf0499dbb74970e8
writetofahim <writetofahimahammad@gmail.com>
2023-07-10 12:30:02 +0600

    modal ready

---
+++ client/src/components/modal/TableModal.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const TableModal = ({ isOpen, onClose, data }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-center justify-center min-h-screen px-4   fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+        <div className="backdrop-blur-md bg-white/50 rounded-lg p-6">
+          <h2 className="text-2xl font-bold mb-4">{data.title}</h2>
+          <p>{data.content}</p>
+          <button
+            className="bg-gray-500 text-white py-2 px-4 rounded mt-4"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TableModal;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import TableModal from "../../components/modal/TableModal";
 
 const Home = () => {
-  return <div className="">Home</div>;
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const modalData = {
+    title: "Modal Title",
+    content: "Modal content goes here",
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div className="">
+      <TableModal isOpen={isModalOpen} onClose={closeModal} data={modalData} />
+      Home
+    </div>
+  );
 };
 
 export default Home;
