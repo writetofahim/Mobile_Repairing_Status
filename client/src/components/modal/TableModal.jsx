1b3ee73a8066534e2616a46eee12c4db6af07a8a
writetofahim <writetofahimahammad@gmail.com>
2023-07-10 12:37:16 +0600

    modal ready again

--- client/src/components/modal/TableModal.jsx
+++ client/src/components/modal/TableModal.jsx
@@ -6,18 +6,16 @@ const TableModal = ({ isOpen, onClose, data }) => {
   }
 
   return (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="flex items-center justify-center min-h-screen px-4   fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
-        <div className="backdrop-blur-md bg-white/50 rounded-lg p-6">
-          <h2 className="text-2xl font-bold mb-4">{data.title}</h2>
-          <p>{data.content}</p>
-          <button
-            className="bg-gray-500 text-white py-2 px-4 rounded mt-4"
-            onClick={onClose}
-          >
-            Close
-          </button>
-        </div>
+    <div className="flex items-center justify-center fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+      <div className="backdrop-blur-md bg-white/50 rounded-lg p-6">
+        <h2 className="text-2xl font-bold mb-4">{data.title}</h2>
+        <p>{data.content}</p>
+        <button
+          className="bg-gray-500 text-white py-2 px-4 rounded mt-4"
+          onClick={onClose}
+        >
+          Close
+        </button>
       </div>
     </div>
   );
